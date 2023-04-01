3f276ffe20395073b356cad5354e3981d423c70a
DQ Griffin <dquaviusgriffin@gmail.com>
2023-03-31 22:22:28 -0400

    Small fixes

--- components/shared/FeaturedProject.tsx
+++ components/shared/FeaturedProject.tsx
@@ -4,7 +4,7 @@ export function FeaturedProject () {
     return (
         <div className='flex flex-col items-center text-center justify-center'>
           <h3 className='text-3xl font-semibold my-4'>Featured project</h3>
-          <div className='flex flex-col justify-center items-center h-56 border-[1px] mx-4 border-gray-400 bg-navbg rounded-lg'>
+          <div className='flex flex-col justify-center items-center max-w-[700px] h-56 border-[1px] mx-4 border-gray-400 bg-navbg rounded-lg'>
             <p className='text-2xl font-semibold mb-2'>Croissant</p>
             <p className='text-sm mb-2 text-gray-400'>React · TypeScript · Material UI · RingCentral API</p>
             <p className='mb-2 px-4'>Croissant is a tool used internally at RingCentral to stand up and support customer accounts</p>
