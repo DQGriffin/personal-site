ec7017200cdc858af57a8ba62c8e20aa17bda647
DQ Griffin <dquaviusgriffin@gmail.com>
2023-04-05 19:39:38 -0400

    Left justify hero text on mobile

--- components/shared/Hero.tsx
+++ components/shared/Hero.tsx
@@ -6,7 +6,7 @@ export function Hero () {
     return (
         <div className='flex justify-center items-center bg-gradient-to-r from-indigo-500 via-purple-500 to-purple-400 dark:from-black dark:to-black text-zinc-50'>
             <div className="lg:container md:mx-auto lg:mx-auto mx-4">
-                <div className='w-full h-64 lg:h-[500px] flex justify-center items-center lg:items-start flex-col'>
+                <div className='w-full h-64 lg:h-[500px] flex justify-center items-start flex-col'>
                     <h2 className='text-2xl lg:text-5xl font-bold mb-2'>Hi, I&apos;m DQ.</h2>
                     <h2 className='text-2xl lg:text-5xl font-bold mb-2'>I build fun things for the web.</h2>
                     <div className='space-x-4 mt-2'>
